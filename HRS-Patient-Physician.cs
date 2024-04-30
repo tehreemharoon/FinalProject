62341bc1c7d14f4bdbeaeba5d5ed4d69557574dc
Tehreem Haroon <tehreem.haroon@marquette.edu>
2024-04-30 17:09:35 -0500

    hrs

---
+++ HRS-Patient-Physician.cs
@@ -0,0 +1,15 @@
+﻿namespace FinalProject;
+
+public class HRS_Patient_Physician
+{
+public Patient patient { get; set; }
+public Physician physician { get; set; }
+public HealthRecordSystem healthrecordsystem { get; set; }
+
+        public HRS_Patient_Physician(Patient pt, Physician phys, HealthRecordSystem hrs)
+        {
+            patient = pt;
+            physician = phys;
+            healthrecordsystem = hrs;
+        }
+}
