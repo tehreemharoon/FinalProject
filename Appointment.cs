42c8058c1664a83f1a4d03b8383460dbd9d46c21
lade <omolade.akande@marquette.edu>
2024-04-30 15:55:27 -0500

    Pushed Ap, Cust, CA, Customers

---
+++ Appointment.cs
@@ -0,0 +1,14 @@
+﻿namespace FinalProject;
+
+public class Appointment
+{
+    private static int autoIncreament;
+        public int Id { get; set; }
+        public DateTime date { get; set; }
+
+        public Appointment()
+        {
+            autoIncreament++;
+            Id = autoIncreament;
+        }
+}
---
+++ Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace FinalProject;
+
+public class Customer
+{
+    private static int autoIncreament;
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Customer()
+        {
+            autoIncreament++;
+            Id = autoIncreament;
+        }
+}
