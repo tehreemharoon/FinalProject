873a80d1d222c76e8562f62f6192c8be63f74a20
lade <omolade.akande@marquette.edu>
2024-04-30 16:41:04 -0500

    Created physician and physicians list

---
+++ Physician.cs
@@ -0,0 +1,20 @@
+﻿namespace FinalProject;
+
+public class Physician
+{
+    private static int autoIncrement;
+    public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string address {get; set;}
+        public string phonenumber {get; set;}
+        public string degree {get; set;}
+        public string position {get; set;}
+
+        public Physician(){
+            autoIncrement++;
+            Id = autoIncrement;
+        }
+}
