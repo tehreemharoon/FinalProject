298f7d6020b11c63ea23474dd4e52b158b36e8f5
lade <omolade.akande@marquette.edu>
2024-04-30 16:05:36 -0500

    Push program

--- Program.cs
+++ Program.cs
@@ -2,9 +2,176 @@
 
 class Program
 {
-    // Lade test comment
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+    private static Customers customers;
+        private static List<Appointment> appointments;
+        private static List<CustomerAppointment> customerAppointments;
+        private static Customer authenticatedCustomer;
+
+        private static Customer customer;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initializing...");
+            Initialize(); // Create and initialize all objects ...
+            Menu();
+        }
+
+        static void Initialize()
+        {
+            var c1 = new Customer
+            {
+                FirstName = "Kambiz",
+                LastName = "Saffari",
+                Username = "kambiz",
+                Password = "1234"
+            };
+            var c2 = new Customer
+            {
+                FirstName = "Terence",
+                LastName = "Ow",
+                Username = "terence",
+                Password = "2345"
+            };
+            var a1 = new Appointment();
+            var a2 = new Appointment();
+            var a3 = new Appointment();
+
+            var ca1 = new CustomerAppointment(c1, a1);
+            var ca2 = new CustomerAppointment(c1, a2);
+            var ca3 = new CustomerAppointment(c2, a3);
+
+            customers = new Customers();
+            customers.customers.Add(c1);
+            customers.customers.Add(c2);
+
+            appointments = new List<Appointment>();
+            appointments.Add(a1);
+            appointments.Add(a2);
+            appointments.Add(a3);
+
+            customerAppointments = new List<CustomerAppointment>();
+            customerAppointments.Add(ca1);
+            customerAppointments.Add(ca2);
+            customerAppointments.Add(ca3);
+
+
+        }
+
+        static void Menu()
+        {
+            bool done = false;
+
+            while (!done)
+            {
+                Console.WriteLine("Options: Login: 1 --- Logout: 2 --- Sign Up: 3 --- Appointments: 4 --- Clear Screen: c --- Quit: q ---");
+                Console.Write("Choice: ");
+                string choice = Console.ReadLine();
+                switch(choice)
+                {
+                    case "1":
+                        LoginMenu();
+                        break;
+                    case "2":
+                        LogoutMenu();
+                        break;
+                    case "3":
+                        SignUpMenu();
+                        break;
+                    case "4":
+                        GetCurrentAppointmentsMenu();
+                        break;
+                    case "c":
+                        Console.Clear();
+                        break;
+                    case "q":
+                        done = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid command!");
+                        break;
+                }
+
+            }
+
+        }
+
+
+        static void LoginMenu()
+        {
+            if(authenticatedCustomer == null)
+            {
+                Console.Write("Enter your username: ");
+                string username = Console.ReadLine();
+                Console.Write("Enter your password: ");
+                string password = Console.ReadLine();
+                authenticatedCustomer = customers.Authenticate(username, password);
+                if (authenticatedCustomer != null)
+                {
+                    Console.WriteLine($"Welcome {authenticatedCustomer.FirstName}");
+                }
+                else
+                {
+                    Console.WriteLine("invalid username or password");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"You are already logged in as {authenticatedCustomer.Username}");
+            }
+        }
+
+        static void LogoutMenu()
+        {
+            authenticatedCustomer = null;
+            Console.WriteLine("Logged out!");
+        }
+
+        static void SignUpMenu()
+        {
+            Console.Write("First Name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Last Name: ");
+            string LastName = Console.ReadLine();
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            var newCustomer = new Customer
+            {
+                FirstName = firstName,
+                LastName = LastName,
+                Username = username,
+                Password = password
+            };
+
+            customers.customers.Add(newCustomer);
+
+            Console.WriteLine("Profile created!");
+
+        }
+
+
+        static void GetCurrentAppointmentsMenu()
+        {
+            if(authenticatedCustomer == null)
+            {
+                Console.WriteLine("You are not logged in.");
+                return;
+            }
+
+
+            var appointmentList = customerAppointments.Where(o => o.customer.Username == authenticatedCustomer.Username);
+
+            if(appointmentList.Count() == 0)
+            {
+                Console.WriteLine("0 appointment found.");
+            }
+            else
+            {
+                foreach(var appointmnet in appointmentList)
+                {
+                    Console.WriteLine(appointmnet.appointment.date);
+                }
+            }
+        }
 }
